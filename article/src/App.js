bce9e235d3c1084ab9d7951b40523c2e759d7c6b
mujibbbb <muhamadqoribulmujib@gmail.com>
2022-04-27 13:39:40 +0700

    Add Reactstrap

--- article/src/App.js
+++ article/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import { Button } from "reactstrap";
+//f8e0c8edaac141c7a18805435a433f39
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
 }
 
 export default App;
